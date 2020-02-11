de65a6128e14727a2007fb5fda04f17043df066d
Rick DeAmicis <rtdeamicis@gmail.com>
2020-02-10 21:40:04 -0800

    moved annoying dictionary definitions plus sensitive sign-in information to t.py.  Began to extend twitter.models.Status class within twitter_toy.py

--- twitter_toy.py
+++ twitter_toy.py
@@ -5,184 +5,178 @@ import requests
 import subprocess as sp
 import datetime, re
 from datetime import time
+from t import ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+from t import dow, month
+from t import date_ordinals as ordinals
 
-
+class tweet(twitter.models.Status):
+    # TODO: wrap the Status class with the below functions
+    pass
 
 def niceSoundingDate(date):
-        dow = {"Mon":"monday",
-               "Tue":"tuesday",
-               "Wed":"wednesday",
-               "Thu":"thursday",
-               "Fri":"friday",
-               "Sat":"saturday",
-               "Sun":"sunday"}
-        month = {"Jan":"january",
-                 "Feb":"february",
-                 "Mar":"march",
-                 "Apr":"april",
-                 "Jun":"june",
-                 "Jul":"july",
-                 "Aug":"august",
-                 "Sep":"septmber",
-                 "Oct":"october",
-                 "Nov":"november",
-                 "Dec":"december"}
-        ordinals = ["first","second","third","fourth","fifth","sixth","seventh",
-                    "eighth","ninth","tenth","eleventh","twelfth","thirteenth",
-                    "fourteenth", "fifteenth","sixteenth","seventeenth",
-                    "eighteenth","nineteenth","twentieth","twenty first",
-                    "twenty second","twenty third", "twenty fourth",
-                    "twenty fifth","twenty sixth","twenty seventh",
-                    "twenty eighth","twenty ninth","thirtieth","thirty first"]
+    date = date.split()
+    date = date[0:3]
+    date[0] = dow[date[0]]
+    date[1] = month[date[1]]
+    date[2] = ordinals[int(date[2])-1]
+    return ' '.join(date)
 
 
-        date = date.split()
-        date = date[0:3]
-        date[0] = dow[date[0]]
-        date[1] = month[date[1]]
-        date[2] = ordinals[int(date[2])-1]
-        return ' '.join(date)
 
 def espeak(text, *args):
-        print(args)
-        args_list = ["espeak-ng"]
-        if args:
-                args_list.extend([text].extend(args))
-        else:
-                args_list.append(text)
-        sp.run(args_list)
+    print(args)
+    args_list = ["espeak-ng"]
+    if args:
+        args_list.extend([text].extend(args))
+    else:
+        args_list.append(text)
+    sp.run(args_list)
 
-def user_name(tweet,i,j):
-        tweet.user_mentions
+
+
+def user_name(user_name,i,j):
+    tweet.user_mentions
         
-        
+
+    
 def audible_text(tweet):
-        original_text = delete_audibles(tweet.full_text)
-        # TODO: format text based on other properties of tweet objec
-        audible_text = ''
-        text_flags = [0]*len(original_text)
-        urls = tweet.urls
-        user_mentions = tweet.user_mentions
-        hashtags = tweet.hashtags
-        # 1 == characters to delete
-        # 2 == user_names to expand from user_name to display names
-        # 3 == hashtags to process into a more readable form
-        for url in urls:
-                i,j = url.indices
-                text_flags[i:j] = [1]*(j-i)
-        for m in user_mentions:
-                i,j = m.indices
-                text_flags[i] = [1]
+    original_text = delete_audibles(tweet.full_text)
+    # TODO: format text based on other properties of tweet object
+    audible_text = ''
+    text_flags = [0]*len(original_text)
+    urls = tweet.urls
+    user_mentions = tweet.user_mentions
+    hashtags = tweet.hashtags
+    # 1 == characters to delete
+    # 2 == user_names to expand from user_name to display names
+    # 3 == hashtags to process into a more readable form
+    for url in urls:
+        i,j = url.indices
+        text_flags[i:j] = [1]*(j-i)
+    for m in user_mentions:
+        i,j = m.indices
+        text_flags[i] = [2]
+        i += 1
+        text_flags[i] = [2, m.name]
+        text_flags[i:j] = [2]*(j-i)
+    for h in hashtags:d
+        i,j = h.hashtags
+        text_flags[i:j] = [3]*(j-1)
+
+    #final processing step
+    for i in range(len(text_flags)):
+        if text_flags[i]==0:
+            audible_text += text[i]
+        elif text_flags[i]==2:
+            start = i
+            while text_flags[i] == 2:
                 i += 1
-                text_flags[i:j] = [2]*(j-i)
-        for h in hashtags:
-                i,j = h.hashtags
-                text_flags[i:j] = [3]*(j-1)
-
-        #final processing step
-        for i in range(len(text_flags)):
-                if text_flags[i]==0:
-                        audible_text += text[i]
-                elif text_flags[i]==2:
-                        start = i
-                        while text_flags[i] == 2:
-                                i += 1
-                        stop = i
-                        audible_text += user_name(original_text,i,j)
-                        new_text += ' '
+                stop = i
+                audible_text += user_name(original_text,i,j)
+                new_text += ' '
         
-        user_mentions = tweet.user_mentions
-        for m in user_mentions:
-                indices = m.indices
-        
+    user_mentions = tweet.user_mentions
+    for m in user_mentions:
+        indices = m.indices
                 
 
-                
-        
+
 def greeting(date):
-        ordinals = ["first","second","third","fourth","fifth","sixth","seventh",
-                    "eighth","ninth","tenth","eleventh","twelfth","thirteenth",
-                    "fourteenth", "fifteenth","sixteenth","seventeenth",
-                    "eighteenth","nineteenth","twentieth","twenty first",
-                    "twenty second","twenty third", "twenty fourth",
-                    "twenty fifth","twenty sixth","twenty seventh",
-                    "twenty eighth","twenty ninth","thirtieth","thirty first"]
-        
-        if date.minute == 0:
-                minute = ""
-        elif date.minute < 10:
-                minute = "oh {}".format(date.minute)
-        else:
-                minute = date.minute
+    if date.minute == 0:
+        minute = ""
+    elif date.minute < 10:
+        minute = "oh {}".format(date.minute)
+    else:
+        minute = date.minute
 
-        dow = date.strftime('%A')
-        month = date.strftime('%B')
-        day = ordinals[date.day - 1]
-        am_pm = date.strftime('%p')
-        if date.hour < 12:
-                greeting = "Good Morning!"
-        elif date.hour < 18:
-                greeting = "Good Afternoon!"
-        else:
-                greeting = "Good Evening!"
+    dow = date.strftime('%A')
+    month = date.strftime('%B')
+    day = ordinals[date.day - 1]
+    am_pm = date.strftime('%p')
+    if date.hour < 12:
+        greeting = "Good Morning!"
+    elif date.hour < 18:
+        greeting = "Good Afternoon!"
+    else:
+        greeting = "Good Evening!"
 
-        hour = date.strftime('%I')
-        if hour.startswith('0'): hour = hour[1]
-        response = "{0} It is {1} {2} {3} on {4}, {5} {6}.".format(greeting, hour, minute, am_pm, dow, month, day)
-        response = re.sub("\s\s+", " ", response)
-        response += " Here are some recent tweets from your timeline"
-        print(response)
-       # espeak(response)
+    hour = date.strftime('%I')
+    if hour.startswith('0'): hour = hour[1]
+    response = "{0} It is {1} {2} {3} on {4}, {5} {6}.".format(greeting,
+                                                               hour, minute,
+                                                               am_pm, dow,
+                                                               month, day)
+    response = re.sub("\s\s+", " ", response)
+    response += " Here are some recent tweets from your timeline"
+    print(response)
+    # espeak(response)
+
+
 
 def date2int(date):
-        return date.year*10000 + date.month*100 + date.day
+    return date.year*10000 + date.month*100 + date.day
+
+
 
 def check_for_holiday(date):
         #TODO: check for holidays and append to greeting
+    pass
+
+
 
 def format_response(tweet):
-        top_level_tweet_text = audible_text(tweet.full_text)
-        if tweet.in_reply_to_status_id:
-                original_tweet = api.GetStatus(tweet.in_reply_to_status_id)
-                original_auth = original_tweet.user.name
-                original_text = remove_http(original_tweet.full_text)
-                response = "{} tweeted {}. ".format(original_auth,original_text)
-                response += "To which {} replied: {}".format(tweet.user.name, top_level_tweet_text)
-        elif tweet.quoted_status:
-                quo_tweet = tweet.quoted_status
-                quo_full_text = remove_http(quo_tweet.full_text)
-                response = "{} quoted {}'s tweet that says {}. And then {} added {}.".format(tweet.user.name,quo_tweet.user.name,quo_full_text,top_level_tweet_text)
-        elif tweet.retweeted_status:
-                retweeted_text = remove_http(tweet.retweeted_status.full_text)
-                response = "{} retweeted the following tweet by {}. {}".format(tweet.user.name, tweet.retweeted_status.user.name, retweeted_text)
-        else:
-                response = "{} tweeted {}".format(tweet.user.name, top_level_tweet_text)
-        return response
+    top_level_tweet_text = audible_text(tweet.full_text)
+    if tweet.in_reply_to_status_id:
+        original_tweet = api.GetStatus(tweet.in_reply_to_status_id)
+        original_auth = original_tweet.user.name
+        original_text = remove_http(original_tweet.full_text)
+        response = "{} tweeted {}. ".format(original_auth,original_text)
+        response += "To which {} replied: {}".format(tweet.user.name,
+                                                     top_level_tweet_text)
+    elif tweet.quoted_status:
+        quo_tweet = tweet.quoted_status
+        quo_full_text = remove_http(quo_tweet.full_text)
+        response = """{} quoted {}'s tweet that says {}. 
+        And then {} added {}.""".format(tweet.user.name,
+                                        quo_tweet.user.name,
+                                        quo_full_text,
+                                        top_level_tweet_text)
+    elif tweet.retweeted_status:
+        retweeted_text = remove_http(tweet.retweeted_status.full_text)
+        response = """{} retweeted the following 
+        tweet by {}. {}""".format(tweet.user.name,
+                                  tweet.retweeted_status.user.name,
+                                  retweeted_text)
+    else:
+        response = "{} tweeted {}".format(tweet.user.name, top_level_tweet_text)
+    return response
+
+
 
 def main(api):
-        tweets = api.GetHomeTimeline(count=20, exclude_replies=True)
-        cur_date = datetime.datetime.now()
-        greeting(cur_date)
-        # add one to current date so we ensure it will always get spoken first time thru for-loop
-        cur_date = date2int(cur_date) + 1
-        for tweet in tweets:
-                tweet_date = date2int(datetime.date.fromtimestamp(tweet.created_at_in_seconds))
-                if tweet_date < cur_date:
-                        spk_date = niceSoundingDate(tweet.created_at)
-                        # espeak(spk_date)
-                        print(spk_date)
-                        cur_date = tweet_date
+    tweets = api.GetHomeTimeline(count=20, exclude_replies=True)
+    cur_date = datetime.datetime.now()
+    greeting(cur_date)
+    # add one to current date so we ensure it will always get spoken first time thru for-loop
+    cur_date = date2int(cur_date) + 1
+    for tweet in tweets:
+        tweet_date = date2int(datetime.date.fromtimestamp(tweet.created_at_in_seconds))
+        if tweet_date < cur_date:
+            spk_date = niceSoundingDate(tweet.created_at)
+            # espeak(spk_date)
+            print(spk_date)
+            cur_date = tweet_date
 
-                response = format_response(tweet)
-                print(response)
-                input("press any key to continue")
-               # espeak(response)
+            response = format_response(tweet)
+            print(response)
+            input("press any key to continue")
+            # espeak(response)
 
 if __name__ == "__main__":
-        api = twitter.Api(consumer_key="Q6wIR9BVePrYP9pnF4rYWRGtn",
-                          consumer_secret="cJJmc2blM6QVSjcaim3R5hDtqNOEHANZcn2iuVw8UrgA1etXX2",
-                          access_token_key="1210385185692217345-juZyflHFepN7gemEjAxhKMoQespSg9",
-                          access_token_secret="l0pDPKJSskdWZ2GZlMfB5GvoiO61ZksOhgO4Dvk09htuh",
-                          tweet_mode='extended')
+    api = twitter.Api(consumer_key=CONSUMR_KEY,
+                      consumer_secret=cCONSUMER_SECRET,
+                      access_token_key=ACCESS_TOKEN_KEY,
+                      access_token_secret=ACCESS_TOKEN_SECRET,
+                      tweet_mode='extended')
         
-        main(api)
+    main(api)
